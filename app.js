4fb9595536ce5f8e57a10423d378c24b5209935b
RaniaAbdullahh <raniaabdullah6600@gmail.com>
2021-12-24 00:34:36 +0200

    firstRout_done

--- app.js
+++ app.js
@@ -11,5 +11,10 @@ app.delete();
 // 1. the path/url/endpoint 
 // 2. callback function () [the function that we will call when we have GET http request ]
 app.get('/', (req,res)=>{
-    
+    res.send('hello World');
 });
+// => so this is how we define a rout, we specify the path ('/') and the callback func or the rout handler 
+// now we need to listen to a given port, takes a port number, and optionally we can pass a function will be called when the app start listening to the giving port
+
+
+app.listen(3000, ()=>console.log('Listening on port 3000...'));
